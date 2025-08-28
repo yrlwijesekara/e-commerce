c348e7c3a9543ac378e24a84e69835685b34abfc
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-08-28 22:20:01 +0530

    Add nodemon for automatic server restarts and update package.json

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,14 @@ import express from 'express';
 
 let app = express();
 
-
+app.get("/",(req,res)=>{
+    console.log("Received a GET request");
+    res.json("Hello World");
+})
+app.post("/",(req,res)=>{
+    console.log("Received a post request");
+    res.json("Hello World from post");
+})
 
 app.listen(5000,()=>{
     console.log("Server started at port 5000");

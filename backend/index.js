daf433e4109c53345dedf92b69b5764a9364fa3f
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-08-30 14:45:33 +0530

    Set up MongoDB connection and update dependencies in package.json

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 
 let app = express();
+let connectionString = "mongodb+srv://yehan:1234@cluster0.tsi6es5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(connectionString).then(()=>{
+    console.log("Connected to MongoDb");
+}).catch((error)=>{
+    console.error("MongoDB connection error:", error);
+});
+
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     console.log("Received a GET request");
     res.json("Hello World");
 })
 app.post("/",(req,res)=>{
-    console.log("Received a post request");
+    console.log(req.body);
     res.json("Hello World from post");
 })
 

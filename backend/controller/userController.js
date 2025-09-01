93ec93b84f799aa0d172199506072fe779edc92c
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-09-01 20:13:52 +0530

    Refactor userController and index to improve code structure and add JWT authorization middleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,76 +3,69 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export function createUser(req, res) {
+  const passwordHash = bcrypt.hashSync(req.body.password, 10);
+  const userData = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: passwordHash,
+  };
+  const user = new User(userData);
 
-     const passwordHash = bcrypt.hashSync(req.body.password, 10);
-     const userData ={
-        firstname : req.body.firstname,
-        lastname : req.body.lastname,
-        email : req.body.email,
-        phone : req.body.phone,
-        password : passwordHash
-        
-     }
-    const user = new User(userData);
-
-    
-    user.save().then(
-        ()=>{
-            res.json({
-                message: "User created successfully",
-                user
-            });
-        }
-    ).catch(
-        (err) => {
-            res.json({
-                message: "Error creating user",
-                error: err.message
-            });
-        }
-           
-    )
-        
-}
-
-
-export function loginUser(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    User.findOne({ email: email }).then((user) => {
-        if (user == null) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        else{
-            const passwordValid = bcrypt.compareSync(password, user.password);
-            if(passwordValid){
-                const token = jwt.sign({
-                    email : user.email,
-                    firstname : user.firstname,
-                    lastname : user.lastname,
-                    role : user.role,
-                    isBlocked : user.isBlocked,
-                    isEmailVerified : user.isEmailVerified,
-                    image : user.image
-
-                },
-                "secret"
-            )
-                res.json({
-                    message: "Login successful",
-                    token: token
-                })
-            }else{
-                res.status(401).json({
-                    message: "Invalid password"
-                });
-            }
-        }
-    }).catch((err) => {
-        return res.status(500).json({ message: "Error logging in", error: err.message });
+  user
+    .save()
+    .then(() => {
+      res.json({
+        message: "User created successfully",
+        user,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: "Error creating user",
+        error: err.message,
+      });
     });
 }
 
+export function loginUser(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
 
-  
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user == null) {
+        return res.status(404).json({ message: "User not found" });
+      } else {
+        const passwordValid = bcrypt.compareSync(password, user.password);
+        if (passwordValid) {
+          const token = jwt.sign(
+            {
+              email: user.email,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              role: user.role,
+              isBlocked: user.isBlocked,
+              isEmailVerified: user.isEmailVerified,
+              image: user.image,
+            },
+            "secret"
+          );
+          res.json({
+            message: "Login successful",
+            token: token,
+          });
+        } else {
+          res.status(401).json({
+            message: "Invalid password",
+          });
+        }
+      }
+    })
+    .catch((err) => {
+      return res
+        .status(500)
+        .json({ message: "Error logging in", error: err.message });
+    });
+}
--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,47 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import Student from './models/student.js';
-import studentRouter from './routers/studentRouter.js';
-import userRouter from './routers/userRouter.js';
-
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import Student from "./models/student.js";
+import studentRouter from "./routers/studentRouter.js";
+import userRouter from "./routers/userRouter.js";
+import jwt from "jsonwebtoken";
 
 const app = express();
-app.use(bodyParser.json());
-const connectionString = "mongodb+srv://yehan:1234@cluster0.tsi6es5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(connectionString).then(()=>{
-    console.log("Connected to MongoDb");
-}).catch((error)=>{
-    console.error("MongoDB connection error:", error);
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  const value = req.header("Authorization");
+  if (value != null) {
+    const token = value.replace("Bearer ", "");
+    jwt.verify(token, "secret", (err, decoded) => {
+      if (decoded == null) {
+        return res.status(401).json({ error: "Unauthorized" });
+      } else {
+        req.user = decoded;
+        
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 });
+
+const connectionString =
+  "mongodb+srv://yehan:1234@cluster0.tsi6es5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
 app.use("/students", studentRouter);
 app.use("/users", userRouter);
 
-app.listen(5000,()=>{
-    console.log("Server started at port 5000");
-}
-
-)
+app.listen(5000, () => {
+  console.log("Server started at port 5000");
+});

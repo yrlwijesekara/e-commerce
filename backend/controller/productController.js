4548ae6a71b99ebba1cd86f09dac653d28f2ed13
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-09-02 13:46:12 +0530

    Add authentication checks to product creation endpoint

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -2,6 +2,18 @@ import Product from "../models/product.js";
 
 
 export async function createProduct(req, res) {
+    if (req.user == null) {
+        return res.status(403).json({
+            message: "please Login first",
+            error: "User not authenticated"
+        });
+    } if (req.user.role !== "admin") {
+        return res.status(403).json({
+            message: "Access denied",
+            error: "User is not an admin"
+        });
+    }
+
     const newProduct = new Product(req.body);
     try {
         const savedProduct = await newProduct.save();

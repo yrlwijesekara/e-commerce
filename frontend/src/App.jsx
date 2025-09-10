bde56c4775185d9728ad2407fe28025516baf0b0
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-09-11 00:19:52 +0530

    remove ProductCard component and update App layout with new design🚀🚀

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,22 @@
+import "./App.css";
 
-import './App.css'
-import ProductCard from './components/productCard'
 function App() {
-  
-
   return (
     <>
-      <div>
-         <ProductCard 
-         name = "samsung galaxy"
-         price = "100"
-         />
-         <ProductCard 
-         name = "Apple iPhone"
-         price = "500"
-         />
+      <div className="w-full h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <div className="w-[600px] h-[500] bg-amber-100 flex flex-row items-center justify-center">
+          <div className="w-[75px] h-[75px] bg-red-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-blue-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-blue-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-blue-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-blue-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-blue-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-yellow-600 text-white flex items-center justify-center rounded-full m-2"></div>
+          <div className="w-[75px] h-[75px] bg-green-600 text-white flex items-center justify-center rounded-full mb-6 mr-2"></div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

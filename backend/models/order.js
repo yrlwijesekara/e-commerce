1be8e6233d05910b1c6dc0e4871624b22ea488ff
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-10-31 15:48:34 +0530

    add Order model with schema definition for order management🚀🚀

---
+++ backend/models/order.js
@@ -0,0 +1,27 @@
+import e from "express";
+import mongoose from "mongoose";
+const orderSchema = new mongoose.Schema(
+    {
+       orderID : { type: String, required: true, unique: true },
+       email : { type: String, required: true },
+       name : { type: String, required: true },
+       address : { type: String, required: true },
+       phone : { type: String, required: true },
+       status : { type: String, required: true, default: "Pending" },
+       date : { type: Date, required: true, default: Date.now },
+       items : [
+        {
+            productId : { type: String, required: true },
+            quantity : { type: Number, required: true },
+            price : { type: Number, required: true },
+            name : { type: String, required: true },
+            image : { type: String, required: true },
+        }
+       ],
+       notes : { type: String, required: "No additional notes" },
+    },
+    { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;

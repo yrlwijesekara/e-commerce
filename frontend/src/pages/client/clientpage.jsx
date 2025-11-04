bf7d8fcc7dc08c1bdb403f870b1dc24990ae1b06
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-11-04 15:16:23 +0530

    add CartPage component and update routing in ClientPage🚀🚀

--- frontend/src/pages/client/clientpage.jsx
+++ frontend/src/pages/client/clientpage.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../../components/header";
 import Product from "./product";
 import ProductOverviewPage from "./productoverviewPage";
+import CartPage from "./cartpage";
 
 export default function ClientPage() {
     return (
@@ -11,7 +12,7 @@ export default function ClientPage() {
                 <Routes>
                     <Route path="/home" element={<h1>Client Home Page</h1>} />
                     <Route path="/products" element={<Product />} />
-                    <Route path="/cart" element={<h1>Cart Page</h1>} />
+                    <Route path="/cart" element={<CartPage />} />
                     <Route path="/about" element={<h1>About Us Page</h1>} />
                     <Route path="/contact" element={<h1>Contact Us Page</h1>} />
                     <Route path="*" element={<h1>404 - Page Not Found</h1>} />

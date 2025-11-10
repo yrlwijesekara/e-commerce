db899ce969c49f9de75b0b4158ac16c00b05cf07
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-11-10 18:28:34 +0530

    refactor CartPage component; enhance layout and styling for improved readability and user interaction👌🚀

--- frontend/src/pages/client/cartpage.jsx
+++ frontend/src/pages/client/cartpage.jsx
@@ -18,24 +18,27 @@ export default function CartPage() {
           return (
             <div
               key={item.productId || index}
-              className="w-[800px] h-[100px] mb-4 rounded overflow-hidden text-black flex flex-row items-center p-4 shadow-md gap-8"
+              className="w-[900px] h-[100px] mb-4 rounded overflow-hidden text-black flex flex-row items-center p-4 shadow-md gap-8 relative"
             >
               <img
                 src={item.image}
                 alt={item.name}
                 className="w-[100px] h-full object-cover rounded mr-4 float-left flex "
               />
-              <div className="w-[300px] flex flex-col justify-center items-center">
-                <h3>{item.name}</h3>
+              <div className="w-[300px] flex flex-col justify-center items-center t-bold">
+                <h3 className="font-semibold text-lg">{item.name}</h3>
                 
                 
               </div>
-              <div className="w-[200px] flex flex-col justify-center items-center">
-                <p>Quantity: {item.quantity}</p>
+              <div className="w-[200px] flex flex-row justify-center items-center">
+                <button className="w-6 h-8 border border-gray-400 rounded flex items-center justify-center font-bold text-lg bg-white hover:bg-gray-100 transition"> + </button>
+                <p className="font-semibold p-2">Quantity: {item.quantity}</p>
+                <button className="w-6 h-8 border border-gray-400 rounded flex items-center justify-center font-bold text-lg bg-white hover:bg-gray-200 transition cursor-pointer"> - </button>
               </div>
               <div className="w-[200px] flex flex-col justify-center items-center">
-                <p>Price: Rs. {item.price}</p>
+                <p className="font-semibold">Price: Rs. {item.price * item.quantity}</p>
               </div>
+              
             </div>
           );
         })
